a89c96949a204a8469b03fe47f546c229eddd61f
Mark <kimzasqwe@naver.com>
2022-11-14 23:52:02 +0900

    프로젝트 세팅
    DB 세팅

---
+++ src/main/java/com/blogv3apiserver/BlogV3ApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.blogv3apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogV3ApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogV3ApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blogv3apiserver/BlogV3ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogv3apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogV3ApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
