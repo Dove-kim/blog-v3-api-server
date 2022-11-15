1b25896204817ba7ceea8aef6a14a8fcf0bbb1b1
Mark <kimzasqwe@naver.com>
2022-11-15 21:28:22 +0900

    기본 세팅
    - JPA세팅
    - logback 세팅
    - h2 테스트 환경세팅

--- src/main/java/com/blogv3apiserver/BlogV3ApiServerApplication.java
+++ src/main/java/com/blogv3apiserver/BlogV3ApiServerApplication.java
@@ -1,9 +1,13 @@
 package com.blogv3apiserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Slf4j
+@EnableJpaRepositories
 public class BlogV3ApiServerApplication {
 
     public static void main(String[] args) {
