f771a321470dd3c13311612b84a6c4c40242b277
Mark <kimzasqwe@naver.com>
2022-11-21 22:05:23 +0900

    set mysql config

--- src/main/java/com/blogv3apiserver/BlogV3ApiServerApplication.java
+++ src/main/java/com/blogv3apiserver/BlogV3ApiServerApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@Slf4j
-@EnableJpaRepositories
 public class BlogV3ApiServerApplication {
 
     public static void main(String[] args) {
